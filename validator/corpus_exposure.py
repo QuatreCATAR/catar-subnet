11fed54518b607c0045ed96f5b513ca41b88f60a
QuatreCATAR <karkof12@gmail.com>
2026-06-12 16:40:05 +0200

    Implement expose_corpus function
    Add function to expose CATAR corpus content

--- validator/corpus_exposure.py
+++ validator/corpus_exposure.py
@@ -1,1 +1,7 @@
+def expose_corpus() -> str:
+    """
+    Retourne le contenu du Corpus CATAR pour exposition au modèle.
+    """
+    with open("corpus/catar_corpus.txt", "r", encoding="utf-8") as f:
+        return f.read()
 
